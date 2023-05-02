7d45360216226931fa91a9eb66543f0492818ffc
David Posavec Kovac <dposaveck20@foi.hr>
2023-05-02 19:28:30 +0200

    repozitorij studenta

---
+++ Evoluation_Manager/Repositories/StudentReposotiry.cs
@@ -0,0 +1,42 @@
+﻿using DBLayer;
+using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager.Repositories
+{
+    public class StudentReposotiry
+    {
+        public string Student GetStudent(int id)
+        {
+            string sql = $"SELECT * FROM students WHERE Id=(id)";
+            DB.OpenConnection();
+            int reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                Student.CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return Student;
+        }
+        public static List<Student> GetStudents()
+        {
+            List<Student> list = new List<Student>();
+        }
+
+        private string Student CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["id"].ToString();
+            string FristName = reader["FirstName"].ToString();
+            string LastName = reader["LastName"].ToString();
+
+        }
+    }
+}
